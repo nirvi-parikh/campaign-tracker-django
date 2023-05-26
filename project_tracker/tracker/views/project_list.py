f49ce3906d249cddca6144f8d88e0abe8b29f4b1
nirvi-parikh <nirvi.parikh@gmail.com>
2023-05-26 16:47:07 -0400

    Initial commit

---
+++ project_tracker/tracker/views/project_list.py
@@ -0,0 +1,8 @@
+# Create your views here.
+# tracker/views.py
+from django.shortcuts import render
+from .models import Project
+
+def project_list(request):
+    projects = Project.objects.all()
+    return render(request, 'project_list.html', {'projects': projects})
